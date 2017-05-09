d52ddd2322217878d9fecdd4dc16d6084f53b8d4
Vital Ryabchinskiy <vital.ryabchinskiy@gmail.com>
2017-05-09 18:51:26 +0000

    Redirect after create

--- app/controllers/dashboard/tasks/new.js
+++ app/controllers/dashboard/tasks/new.js
@@ -4,7 +4,9 @@ export default Ember.Controller.extend({
 	actions: {
 		save(task) {
 			let newTask = task;
-			newTask.save().catch((error) => {})
+			newTask.save().catch((error) => {}).then(() =>{
+				this.transitionToRoute('dashboard.tasks')
+			})
 		}
 	}
 });
